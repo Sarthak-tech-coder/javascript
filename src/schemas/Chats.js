d7319162bee759e56318fe3dd8ba0f8082599686
Sarthak-tech-coder <69297549+Sarthak-tech-coder@users.noreply.github.com>
2023-06-26 23:04:39 +0530

    Create Chats.js

---
+++ src/schemas/Chats.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    Users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ],
+    FirestoreID: {
+        required: true,
+        unique: true,
+        type: mongoose.Schema.Types.String
+    },
+    type: {
+        type: mongoose.Schema.Types.String,
+        default: "pgroup"
+    },
+    data: {
+        icon: {
+            type: mongoose.Schema.Types.String,
+        },
+        Name: {
+            type: mongoose.Schema.Types.String
+        },
+        subtitle: { type: mongoose.Schema.Types.String }
+    },
+    adminOnly: { type: mongoose.Schema.Types.Boolean, default: false },
+    adminUsers: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    verified: { type: mongoose.Schema.Types.Boolean, default: false },
+})
+module.exports = mongoose.model('Chat', chatSchema)
